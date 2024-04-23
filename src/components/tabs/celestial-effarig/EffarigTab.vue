c14cf4221d9b28e840d5935848286c7d56169f87
FreakyFreakyNerd <freakyfreakynerd@gmail.com>
2024-04-22 19:30:40 -0700

    effarig decimalized I think

--- src/components/tabs/celestial-effarig/EffarigTab.vue
+++ src/components/tabs/celestial-effarig/EffarigTab.vue
@@ -12,14 +12,14 @@ export default {
   },
   data() {
     return {
-      relicShards: 0,
-      shardRarityBoost: 0,
+      relicShards: new Decimal(0),
+      shardRarityBoost: new Decimal(0),
       shardPower: 0,
-      shardsGained: 0,
-      currentShardsRate: 0,
-      amplification: 0,
-      amplifiedShards: 0,
-      amplifiedShardsRate: 0,
+      shardsGained: new Decimal(0),
+      currentShardsRate: new Decimal(0),
+      amplification: new Decimal(0),
+      amplifiedShards: new Decimal(0),
+      amplifiedShardsRate: new Decimal(0),
       runUnlocked: false,
       quote: "",
       isRunning: false,
@@ -70,13 +70,13 @@ export default {
   methods: {
     update() {
       this.relicShards = Currency.relicShards.value;
-      this.shardRarityBoost = Effarig.maxRarityBoost / 100;
+      this.shardRarityBoost = Effarig.maxRarityBoost.div(100);
       this.shardPower = Ra.unlocks.maxGlyphRarityAndShardSacrificeBoost.effectOrDefault(1);
       this.shardsGained = Effarig.shardsGained;
-      this.currentShardsRate = (this.shardsGained / Time.thisRealityRealTime.totalMinutes);
+      this.currentShardsRate = this.shardsGained.div(Time.thisRealityRealTime.totalMinutes);
       this.amplification = simulatedRealityCount(false);
-      this.amplifiedShards = this.shardsGained * (1 + this.amplification);
-      this.amplifiedShardsRate = (this.amplifiedShards / Time.thisRealityRealTime.totalMinutes);
+      this.amplifiedShards = this.shardsGained.times(this.amplification.add(1));
+      this.amplifiedShardsRate = this.amplifiedShards.div(Time.thisRealityRealTime.totalMinutes);
       this.quote = Effarig.quote;
       this.runUnlocked = EffarigUnlock.run.isUnlocked;
       this.isRunning = Effarig.isRunning;
