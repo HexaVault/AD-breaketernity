94919e67dfed4e71f104646e4243a630b06db846
James103 <37945304+James103@users.noreply.github.com>
2024-07-16 20:42:51 -0500

    Fix #23: Tesseract effect was amplified over time
    Due to the way the Infinity Dimensions tab initialized its variables, staying on the tab and maxing out the number of purchases for an Infinity Dimension caused the base purchase cap to increase by the bonus from Tesseracts.
    
    The net cause was a reactivity error that was not directly caught in my testing setup.
    
    I've fixed this by initializing the relevant variable in a way that doesn't link or induce reactivity.

--- src/components/tabs/infinity-dimensions/ModernInfinityDimensionRow.vue
+++ src/components/tabs/infinity-dimensions/ModernInfinityDimensionRow.vue
@@ -33,7 +33,7 @@ export default {
       capIP: new Decimal(0),
       isAutobuyerOn: false,
       isEC8Running: false,
-      hardcap: InfinityDimensions.HARDCAP_PURCHASES,
+      hardcap: new Decimal(InfinityDimensions.HARDCAP_PURCHASES),
       eternityReached: false,
       enslavedRunning: false,
     };
