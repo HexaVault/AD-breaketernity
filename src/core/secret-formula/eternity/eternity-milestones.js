c5d1d3d6d8d8915ae85e448a31ec7f014575db55
antimatter19728 <antimatter19728@163.com>
2024-08-12 14:52:53 +0800

    Update eternity-milestones.js

--- src/core/secret-formula/eternity/eternity-milestones.js
+++ src/core/secret-formula/eternity/eternity-milestones.js
@@ -6,7 +6,7 @@ export const eternityMilestones = {
   },
   keepAutobuyers: {
     eternities: 2,
-    reward: "You start Eternity with all Normal Challenges complete, all normal autobuyers, and infinity broken"
+    reward: "You start Eternity with all Normal Challenges complete and all normal autobuyers"
   },
   autobuyerReplicantiGalaxy: {
     eternities: 3,
