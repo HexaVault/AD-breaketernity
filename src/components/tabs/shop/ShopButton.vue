719df581329d4b4fd1bc48221b34e6b88e0e4e7d
Donaldino7712 <100215778+Donaldino7712@users.noreply.github.com>
2026-05-02 17:47:06 +0200

    bringing shop code up to "refactor code quality" (build fix)

--- src/components/tabs/shop/ShopButton.vue
+++ src/components/tabs/shop/ShopButton.vue
@@ -111,7 +111,7 @@ export default {
     >
       Cost: {{ format(cost, 2) }}
       <img
-        src="images/std_coin.png"
+        src="/images/std_coin.png"
         class="o-shop-button-button__img"
       >
     </button>
